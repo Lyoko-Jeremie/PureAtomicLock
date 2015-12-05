2702edfb90f36b38d8d4ed16329b53548e0e4d3b
Jeremie <lucheng989898@gmail.com>
2015-12-06 03:21:17 +0800

    GCC移植 性能调优 算法调参数

--- PureAtomicLock/AtomicLock.h
+++ PureAtomicLock/AtomicLock.h
@@ -5,8 +5,11 @@
 #include <chrono>
 #include <thread>
 
+//#include <iostream>
+
 // 警告 不能用此对自旋锁象代替标准库的mutex使用，此实现没有内存屏障特性，不能保证内存可见性和前后语句的顺序一致性
-// 
+// 此实现以牺牲一致性与适用性换取性能
+//
 // sleeptime指定自旋等待时间  若sleeptime==0则使用this_thread::yield作为自选等待
 // 默认参数为0
 template<int sleeptime = 0>
@@ -21,7 +24,7 @@ public:
 	void lock()
 	{
 		bool Old = false;
-		while (!this->AB.compare_exchange_strong(Old, true))
+		while (!this->AB.compare_exchange_strong(Old, true))	// 使用CAS模式保证原子性
 		{	// 总是检查AB是否为false  为false则获取锁  否则重试
 			Old = false;
 			this->sleep<sleeptime>();
@@ -41,18 +44,32 @@ public:
 	}
 
 private:
+	// 实际的内核对象  在大部分平台下的实现呈现出lock-free特性
 	std::atomic_bool AB;
 
+	// 两种不同的延迟实现 使用模板参数在编译期决定实际使用的版本 以避免增加运行时时间消耗
+	// 使用偏特化方式自动选择
+	// 缺陷 在编译期便确定下了延迟时间 无法在运行时自动配置延迟
 	template<int s>
 	void sleep()
 	{
+		//std::cout << "1";
 		std::this_thread::sleep_for(std::chrono::milliseconds(s));
 	}
-	template<>
-	void sleep<0>()
-	{
-		std::this_thread::yield();
-	}
 };
+
+
+// 偏特化版本在这里
+// GCC不支持VS版本的在类内部进行的偏特化处理  标准写法要写在类外部
+template<>	// 类的模板参数
+template<>	// 函数的模板参数
+inline		// 内联表示
+void		// 返回参数
+AtomicLock<0>	// 类模板特化
+::sleep<0>()	// 函数模板特化
+{
+	//std::cout << "0";
+	std::this_thread::yield();
+}
 
 #endif	// ATOMICLOCK_H
--- PureAtomicLock/PureAtomicLock.cpp
+++ PureAtomicLock/PureAtomicLock.cpp
@@ -11,17 +11,20 @@
 #include <vector>
 using namespace std;
 
-AtomicLock<> AL;
-size_t a = 0;
+AtomicLock<5> AL; // 使用默认模板参数
+long long int a = 0;
 vector<thread> pt;
 
 void th();
 
 int main()
 {
-	for (int i = 0; i != 100; ++i)
+	cout << "is_lock_free : " << AL.is_lock_free();
+	for (int i = 0; i != 10000; ++i)
+		// 3K是个坎 3K以下可以用0 3K开始0就会活锁 3K时使用参数5 耗时略微小于线性增长
+		// VS实现的yield比GCC性能好但sleep_for相反
 	{
-		pt.emplace_back(th);
+		pt.emplace_back(th);	// 就地构造
 	}
 	for (size_t i = 0; i != pt.size(); ++i)
 	{
@@ -29,7 +32,7 @@ int main()
 	}
 	cout << endl;
 	cout << a << endl;
-	system("pause");
+	//system("pause");
     return 0;
 }
 
@@ -40,13 +43,15 @@ void th()
 	//	lock_guard<decltype(AL)> alg(AL);
 	//	cout << "lock free : " << AL.is_lock_free() << endl;
 	//}
-	for (size_t i = 0; i != 1000; i++)
+	for (size_t i = 0; i != 100; i++)
 	{
-		lock_guard<decltype(AL)> alg(AL);
+		lock_guard<decltype(AL)> alg(AL);	// decltype编译期自动分析对象类型
 		//cout << "th()" << endl;
 		++a;
 		//std::this_thread::sleep_for(std::chrono::milliseconds(2));
+		std::this_thread::yield();
 		--a;
+		std::this_thread::yield();
 	}
 }
 
